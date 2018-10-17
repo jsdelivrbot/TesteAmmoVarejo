06d2cd076dbd39aadc8a2d59ae0131e82c9c8b11
LJLeandro <lucasjleandro@gmail.com>
2018-10-17 00:01:35 -0300

    agora vai

--- front-end/server.js
+++ front-end/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+var path = require('path');
+var express = require('express');
 const app = express();
-const path = require('path');
-
+ 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+app.listen(process.env.PORT);
 
-app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+  res.sendFile('dist/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
